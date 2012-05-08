57683763628485633f02bf965e82ae71b0f9e368
David Birch <birch.dave@gmail.com>
2012-05-08 20:25:40 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MKTW::Application.config.secret_token = '2d4460066a4b25b20dab10c38c057125e7fad63cecdc05414726499302c6eafeb07d2e4a2b905a97d16b40752523c58b369854a3d3cb655496066a01fc5c6805'
