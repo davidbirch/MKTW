8319ccf701ee50008be19a9349b1e4efc47bf681
David Birch <birch.dave@gmail.com>
2012-06-15 17:34:50 +1000

    basic monitoring for new and parsing into the db

--- app/models/tag.rb
+++
@@ -1,6 +0,0 @@
-class Tag < ActiveRecord::Base
-  
-  # associations
-  belongs_to :tweet
-  
-end
--- app/models/tweet.rb
+++
@@ -1,6 +0,0 @@
-class Tweet < ActiveRecord::Base
-  
-  # associations
-  has_many :tags, :dependent => :destroy
-  
-end
